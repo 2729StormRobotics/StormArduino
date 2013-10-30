0e4d11311376d886c0d13c7fff90a4a4e82750c3
Evan Allan <evanallan1026@gmail.com>
2013-10-29 21:51:18 -0400

    Integrating example code into my code. Now just need to test.

--- LEDControl/LEDControl.ino
+++ LEDControl/LEDControl.ino
@@ -1,30 +1,12 @@
+//see example code for wiring
+
 #define LEDCount 32 //If the strip doesn't change
 
-class LED {
-  public: 
-    int r;
-    int g;
-    int b;
+long leds[LEDCount];
 
-    LED(){
-      r = 0;
-      g = 0;
-      b = 0;
-    }
-
-    LED(int _r, int _g, int _b){
-      r = _r;
-      g = _g;
-      b = _b; 
-    }
-
-    void setColor(int _r, int _g, int _b){
-      r = _r;
-      g = _g;
-    }
-};
-
-LED leds[LEDCount];
+int SDI = 2; //Red wire (not the red 5V wire!)
+int CKI = 3; //Green wire
+int ledPin = 13; //On board LED
 
 class LEDMode {
   public:
@@ -38,8 +20,8 @@ class Idle : public LEDMode {
   public:
     void initialize() {
       for (int i = 0; i < LEDCount; i++){
-        if (i % 3 == 0) leds[i].setColor(255, 255, 255);
-        else            leds[i].setColor(  0,   0,   0);
+        if (i % 3 == 0) leds[i] = 0xFFFFFF;
+        else            leds[i] = 0x000000;
       }
     }
     
@@ -48,8 +30,8 @@ class Idle : public LEDMode {
       count = count % 3;
   
       for (int i = 0; i < LEDCount; i++){
-        if ((i + count) % 3 == 0) leds[i].setColor(255, 255, 255);
-        else                      leds[i].setColor(  0,   0,   0);
+        if ((i + count) % 3 == 0) leds[i] = 0xFFFFFF;
+        else                      leds[i] = 0x000000;
       }
     }
 };
@@ -62,5 +44,37 @@ void setup(){
 
 void loop(){
   currentMode->doLoop();
+  post_frame();
 }
 
+//Takes the current strip color array and pushes it out
+void post_frame (void) {
+  //Each LED requires 24 bits of data
+  //MSB: R7, R6, R5..., G7, G6..., B7, B6... B0 
+  //Once the 24 bits have been delivered, the IC immediately relays these bits to its neighbor
+  //Pulling the clock low for 500us or more causes the IC to post the data.
+
+  for(int LED_number = 0 ; LED_number < LEDCount ; LED_number++) {
+    long this_led_color = leds[LED_number]; //24 bits of color data
+
+    for(byte color_bit = 23 ; color_bit != 255 ; color_bit--) {
+      //Feed color bit 23 first (red data MSB)
+      
+      digitalWrite(CKI, LOW); //Only change data when clock is low
+      
+      long mask = 1L << color_bit;
+      //The 1'L' forces the 1 to start as a 32 bit number, otherwise it defaults to 16-bit.
+      
+      if(this_led_color & mask) 
+        digitalWrite(SDI, HIGH);
+      else
+        digitalWrite(SDI, LOW);
+  
+      digitalWrite(CKI, HIGH); //Data is latched when clock goes high
+    }
+  }
+
+  //Pull clock low to put strip into reset/post mode
+  digitalWrite(CKI, LOW);
+  delayMicroseconds(500); //Wait for 500us to go into reset
+}
