e06b21ad5dae322e34184cedacd44f9f10767a95
Evan Allan <evanallan1026@gmail.com>
2013-11-07 22:58:38 -0500

    Adding new modes. Can't wait to get my hands on the strip.

--- LEDControl/LEDControl.ino
+++ LEDControl/LEDControl.ino
@@ -4,6 +4,15 @@
 
 long leds[LEDCount];
 
+long WHITE   = 0xFFFFFF;
+long BLACK   = 0x000000;
+long RED     = 0xFF0000;
+long GREEN   = 0x00FF00;
+long BLUE    = 0x0000FF;
+long YELLOW  = 0xFFFF00;
+long MAGENTA = 0xFF00FF;
+long CYAN    = 0x00FFFF;
+
 int SDI = 2; //Red wire (not the red 5V wire!)
 int CKI = 3; //Green wire
 int ledPin = 13; //On board LED
@@ -14,29 +23,98 @@ class LEDMode {
     virtual void doLoop();
 };
 
-class Idle : public LEDMode {
+class Marquee : public LEDMode {
   int count = 0;
 
   public:
-    void initialize() {
-      for (int i = 0; i < LEDCount; i++){
-        if (i % 3 == 0) leds[i] = 0xFFFFFF;
-        else            leds[i] = 0x000000;
-      }
-    }
-    
-    void doLoop(){
-      count++;
-      count = count % 3;
+    void initialize() {}
+    void doLoop() {
   
       for (int i = 0; i < LEDCount; i++){
-        if ((i + count) % 3 == 0) leds[i] = 0xFFFFFF;
-        else                      leds[i] = 0x000000;
+        if ((i + count) % 3 == 0) leds[i] = WHITE;
+        else                      leds[i] = BLACK;
       }
+
+      count++;
+      count %= 3;
     }
 };
 
-LEDMode* currentMode = new Idle();
+class ColorCycle : public LEDMode {
+  int count = 0;
+  int delayCounter = 0; //To make it not change every time it cycles, which is 20 times per second
+
+  public:
+    void initialize() {}
+    void doLoop(){
+      if (delayCounter == 0) {
+        for (int i = 0; i < LEDCount; i++){
+          switch (count){
+            case 0:  leds[i] = WHITE;
+                     break;
+            case 1:  leds[i] = RED;
+                     break;
+            case 2:  leds[i] = GREEN;
+                     break;
+            case 3:  leds[i] = BLUE;
+                     break;
+            case 4:  leds[i] = YELLOW;
+                     break;
+            case 5:  leds[i] = MAGENTA;
+                     break;
+            case 6:  leds[i] = CYAN;
+                     break;
+            default: break;
+          }
+        }
+        count++;
+        count %= 7;
+      }
+      
+      delayCounter++;
+      delayCounter %= 20; //Cycle once per second
+    }
+};
+
+class Pew : public LEDMode {
+  int count = 0;
+  int colorCounter = 0;
+
+  public:
+    void initialize() {}
+    void doLoop() {
+      
+      long currentColor;
+
+      switch (colorCounter){
+        case 0:  currentColor = WHITE;
+                 break;
+        case 1:  currentColor = RED;
+                 break;
+        case 2:  currentColor = GREEN;
+                 break;
+        case 3:  currentColor = BLUE;
+                 break;
+        default: break;
+      }
+
+      for (int i = 0; i < LEDCount; i++){
+        if (i == 15 - count || i == 16 + count) leds[i] = currentColor;
+        else                                    leds[i] = BLACK;
+      }
+
+      count++;
+      count %= 15;
+
+      if (count == 0) {
+        colorCounter++;
+        colorCounter %= 4;
+      }
+    }
+
+};
+
+LEDMode* currentMode = new Marquee();
 
 void setup(){
   currentMode->initialize();
@@ -45,6 +123,8 @@ void setup(){
 void loop(){
   currentMode->doLoop();
   post_frame();
+
+  //TODO changing modes
 }
 
 //Takes the current strip color array and pushes it out
