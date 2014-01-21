4f5ccf9a3eb68c2a4a519c0336f18b561dbcc642
Evan Allan <evanallan1026@gmail.com>
2014-01-21 12:58:35 -0500

    Updated rainbowdanceparty

--- LEDControl/RainbowDanceParty.h
+++ LEDControl/RainbowDanceParty.h
@@ -11,30 +11,41 @@ class RainbowDanceParty : public LEDMode {
     public:
         void doLoop(){
             if (count == 0){
-                for (int i = NUM_LEDS; i > 1; i--){
-                    leds[i] = leds[i - 2];
+                for (int i = NUM_LEDS; i > 0; i--){
+                    leds[i] = leds[i - 1];
                 }
                 switch (colorCounter){
-                    case 0:  leds[0] = RED;
+                    case 0:
+                    case 1:
+                             leds[0] = RED;
                              break;
-                    case 1:  leds[0] = ORANGE;
+                    case 2:
+                    case 3:
+                             leds[0] = ORANGE;
                              break;
-                    case 2:  leds[0] = YELLOW;
+                    case 4:
+                    case 5:
+                             leds[0] = YELLOW;
                              break;
-                    case 3:  leds[0] = GREEN;
+                    case 6:
+                    case 7:
+                             leds[0] = GREEN;
                              break;
-                    case 4:  leds[0] = BLUE;
+                    case 8:
+                    case 9:
+                             leds[0] = BLUE;
                              break;
-                    case 5:  leds[0] = PURPLE;
+                    case 10:
+                    case 11:
+                             leds[0] = PURPLE;
                              break;
                     default: break;
                 }
-                leds[1] = leds[0];
-    
+
                 colorCounter++;
-                colorCounter %= 6;
+                colorCounter %= 12;
             }
-            
+
             count++;
             count %= 2;
         }
